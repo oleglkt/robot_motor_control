9684493755eed901ac92db7ad0516520d0119129
Oleg Loskutov <oleg.lkt@mail.ru>
2024-01-15 12:01:29 +0500

    Add README.md and RobotMotorsControl.py

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-lkenmce
-ssxsxsxsxsx
-jiyhgujyhguyg
-khbkhg
+# Simple primitive code for robot motors control
+
+Мною были использованы команды:
+
+git remote add
+git remote show
+git status
+git commit
+git add
+git config 
+git load
---
+++ RobotMotorsControl.py
@@ -0,0 +1,15 @@
+
+import RPi.GPIO as GPIO
+import time
+GPIO_PWM_0 = 12
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PWM_0, GPIO.OUT)
+GPIO.PWM(GPIO_PWM_0, 50).start(70)
+
+#for i in range(1,100):
+#    GPIO.PWM(GPIO_PWM_0, 50).start(i)
+#    time.sleep(0.1)
+    
+    
